6a54f28d3e22e95445d70161abdced0c80e9a068
杉菜 <dickqi87@gmail.com>
2019-03-07 09:00:47 +0800

    no message

--- apps/keke/views.py
+++ apps/keke/views.py
@@ -9,8 +9,8 @@ import datetime
 class kekeView(APIView):
     def get(self, requests):
         weekday = datetime.datetime.now().weekday() + 1
-        keke = kkTable.objects.filter(week=weekday)
-        xixi = xxTable.objects.filter(week=weekday)
+        keke = kkTable.objects.filter(week=weekday).order_by('beginTime', 'endTime')
+        xixi = xxTable.objects.filter(week=weekday).order_by('beginTime', 'endTime')
         return JsonResponse({
             'keke':[model_to_dict(kk) for kk in keke],
             'xixi':[model_to_dict(xx) for xx in xixi],
